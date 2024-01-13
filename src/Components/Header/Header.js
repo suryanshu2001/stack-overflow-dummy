970ff1d9ef6a628f78b65ccc706abe88c7b5b8fe
Suryanshu Bedi <ece.20becc07@silicon.ac.in>
2024-01-14 01:55:56 +0530

    Header element completed.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,73 +1,29 @@
 import React from 'react'
+import '../Header/Header.css';
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
    <>
    <nav className="navbar navbar-expand-lg  navbar-expand-md navbar-light p-4">
-				<div className="outer-container">
+				{/* <div className="outer-container"> */}
 					<div className="logo-container col-2">
 						<Link to="/">
-							<span
-								style={{
-									fontSize: "3vw",
-									textDecorationColor: "black",
-								}}
-							>
-								LOGO
-							</span>
+							
+								<img className='logo' src='https://stackoverflow.design/assets/img/logos/so/logo-stackoverflow.svg' alt='LOGO' />
+							
 						</Link>
 					</div>
-					<div
-						className="content-container col-9"
-						style={{
-							display: "flex",
-							justifySelf: "center",
-							justifyContent: "space-between",
-						}}
-					>
-						
-
-						<div
-							style={{
-								borderRadius: "1.5vw",
-								backgroundColor: "white",
-								width: "22vw",
-								height: "3.6vh",
-								display: "flex",
-								justifyContent: "flex-start",
-								alignItems: "center",
-								overflow: "hidden",
-							}}
-						>
-							<div
-								style={{
-									fontSize: "1.2vw",
-									marginLeft: "0.8vw",
-									width: "18.2vw",
-									opacity: "0.6",
-									fontWeight: "400px",
-								}}
-							>
-								Search for style,collection
-							</div>
-							<div
-								style={{
-									backgroundColor: "black",
-									height: "3.8vh",
-									width: "6vw",
-									opacity: "0.83",
-								}}
-							/>
+					<div className="content-container col-9">
+						<div className='input-div'>
+                            <img className='search-icon' src={require('../../Assets/Images/searchicon.png') } alert='search button' />
+							<input type='text' className='search-bar' placeholder='Search'/>		
 						</div>
-						<div className="home-container"></div>
-						<div className="home-container">MEN</div>
-						<div className="home-container">WOMEN</div>
-						<div className="home-container">KIDS</div>
-						<div className="home-container">COLLECTION</div>
-						<div className="home-container">TRENDS</div>
+						<div className="header-title">About</div>
+						<div className="header-title">Product</div>
+						<div className="header-title">For teams</div>
 					</div>
-				</div>
+				{/* </div> */}
 			</nav>
    </>
   )
--- src/Routing/ComponentsRout.js
+++ src/Routing/ComponentsRout.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import About from '../Pages/About/About'
 import ForTeams from '../Pages/ForTeams/ForTeams'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home/Home'
 
-function Componentsrout() {
+function ComponentsRout() {
   return (
     <Routes>
 			<Route path="/" exact={true} element={<Home />} />
 			<Route path="/about" element={<About />} />
 			<Route path="/forteams" element={<ForTeams/>} />
-	
 		</Routes>
   )
 }
 
-export default Componentsrout
+export default ComponentsRout
