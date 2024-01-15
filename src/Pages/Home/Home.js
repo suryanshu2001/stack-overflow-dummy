e02b16311111acb8f6caa72fe78cd8c72a4b5fd3
Suryanshu Bedi <ece.20becc07@silicon.ac.in>
2024-01-16 02:53:16 +0530

    frontend design implimentation completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,10 @@ import RightSidebar from '../../Components/RightSidebar/RightSidebar'
 
 function Home() {
   const [issaved,setIssaved] = useState(false)
+  const [selectedItem,setSelectedItem] = useState('Votes'); 
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
   return (
     <div className='home-container'>
         <LeftSidebar />
@@ -46,9 +50,9 @@ function Home() {
                   </span>
                   <div className='question-body-section4'>
                     { !issaved ?
-                      <div className='save-div'><img className='save-image' src={require('../../Assets/Images/savewhite.png')} alt='save'/> 20</div>
+                      <div onClick={()=> setIssaved(!issaved)} className='save-div-u'><img className='save-image' src={require('../../Assets/Images/savewhite.png')} alt='save'/> 20</div>
                       : 
-                      <div className='save-div'><img className='save-image' src={require('../../Assets/Images/savecolored.png')} alt='save'/> 20</div>
+                      <div onClick={()=> setIssaved(!issaved)} className='save-div-s'><img className='save-image' src={require('../../Assets/Images/savecolored.png')} alt='save'/> 21</div>
                     }
                     <div className='bottom-right-image'>
                       <img className='bottom-right-image' src={require('../../Assets/Images/forwardarrow.png')} alt='forward' />
@@ -57,9 +61,53 @@ function Home() {
                   </div>
                 </div>
            </div>
-        </div>
-        <RightSidebar />
+           <div className='home-center-separation' >
+            <div className='answer-counts'>
+              <span className='number'>20</span>
+              <span className='answer-name'>Answers</span>
+            </div>
+         <div className='separation-right-side'>
+        <span
+          className={`home-center-separation-item ${selectedItem === 'Votes' ? 'active' : ''}`}
+          onClick={() => handleItemClick('Votes')}
+        >
+          Votes
+        </span>
+        <span
+          className={`home-center-separation-item ${selectedItem === 'Oldest' ? 'active' : ''}`}
+          onClick={() => handleItemClick('Oldest')}
+        >
+          Oldest
+        </span>
+        <span
+          className={`home-center-separation-item ${selectedItem === 'Active' ? 'active' : ''}`}
+          onClick={() => handleItemClick('Active')}
+        >
+          Active
+        </span>
     </div>
+  </div>
+  <div className='home-center-answer'>
+    <div className='answer-header-section'>
+      <div className='upper-header'>
+        <span className='answer-title'>
+          <span className='answer-profile'>Author:<span className='link'>javed (308)</span></span>
+            <span className='answer-timestamp'>Answered: Dec 9 '19 at 8:20</span>
+        </span>
+        <div className='change-answer-container'>
+          <span className='negative-sign2'>-</span>
+          <span className='count2'>25</span>
+          <span className='posetive-sign2'>+</span>
+        </div>
+    </div>
+    </div>
+    <div className='answer-body'>
+    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+    </div>
+  </div>
+</div>
+<RightSidebar />
+</div>
   )
 }
 
